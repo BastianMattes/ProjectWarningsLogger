049c7f0b42d48500dc81bdb9202a4eab77a0b8be
Bastian Mattes <bastian.mattes@infoteam.de>
2023-05-30 17:09:43 +0200

    Add logger class

---
+++ ProjectWarningsLogger/ProjectWarningsLogger.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ProjectWarningsLogger
+{
+    public class ProjectWarningsLogger : Logger
+    {
+        private static readonly Dictionary<string, int> _projectWarningCountDictionary = new Dictionary<string, int>();
+
+        public override void Initialize(IEventSource eventSource)
+        {
+            eventSource.WarningRaised += OnWarningRaised;
+            eventSource.BuildFinished += OnBuildFinished;
+        }
+
+        private void OnWarningRaised(object sender, BuildWarningEventArgs e)
+        {
+            var projectName = Path.GetFileNameWithoutExtension(e.ProjectFile);
+            if (!_projectWarningCountDictionary.ContainsKey(projectName))
+            {
+                _projectWarningCountDictionary[projectName] = 1;
+            }
+            else
+            {
+                _projectWarningCountDictionary[projectName] += 1;
+            }
+        }
+
+        private void OnBuildFinished(object sender, BuildFinishedEventArgs e)
+        {
+            foreach (var kvp in _projectWarningCountDictionary)
+            {
+                Console.WriteLine($"::warning title=[{kvp.Key}]::{kvp.Value} warnings found in project.");
+            }
+        }
+    }
+}
